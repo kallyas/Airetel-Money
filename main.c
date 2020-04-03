bf7fa87f69decf25ddcc2b7cfacb4256da3b2ffc
Iden Morisha <identumu@gmail.com>
2020-04-03 11:55:13 +0000

    added instructions

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
+
+program written by Iden!
 *******************************************************************************/
 
 #include <stdio.h>
